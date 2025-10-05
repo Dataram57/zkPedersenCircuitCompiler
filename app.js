f076e0726c6800d026e5ce1ab91bc0982e3afd14
Dataram57 <dataram66657@gmail.com>
2025-10-05 20:00:04 +0200

    is secret a bit check example

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const commit = (secret, rand, g, h, p) => multi(powermod(g, secret,p), powermod(
 
 
 //returns a squared commitment
-const commit_squared = (secret, t1, g, h, p) => {
+const commit_squared = (secret, t1, g, h, p, q) => {
     //get current commit
     let c1 = commit(secret, t1, g, h, p);
 
@@ -37,9 +37,9 @@ const commit_squared = (secret, t1, g, h, p) => {
 
     let k = 666666n;    //can use fiat shamir
 
-    let z1 = (multi(k, secret, p) + r1) % p;
-    let z2 = (multi(k, t1, p) + r2) % p;
-    let z3 = (multi(k, t2, p) + r3) % p;
+    let z1 = (multi(k, secret, q) + r1) % q;
+    let z2 = (multi(k, t1, q) + r2) % q;
+    let z3 = (multi(k, t2, q) + r3) % q;
 
     //verification
     //let check1 = multi(c3, powermod(c1, k, p), p) == commit(z1, z2, g, h, p);
@@ -48,11 +48,12 @@ const commit_squared = (secret, t1, g, h, p) => {
 
     return {
         out:{
-            secret: multi(secret, secret, p),
-            t: (multi(secret, t1, p) + t2) % p,
+            secret: multi(secret, secret, q),   //maybe q
+            t: (multi(secret, t1, q) + t2) % q,
             g: g,
             h: h,
             p: p,
+            q: q,
             commit: c2 // commit(secret_new, t_new, g, h, p)
         },
         env:{
@@ -102,6 +103,46 @@ const verify_SquareProof = (env, proof) => {
     return check1 && check2;
 };
 
+const bond_equal_commits = (c1, t1, c2, t2, g, h, p, q) => {
+    //calculate envs
+    //const c1 = commit(secret, t1, g, h, p);
+    //const c2 = commit(secret, t2, g, h, p);
+    
+    //calculate difference
+    const r = (t1 - t2 + q) % q;
+    
+    //result
+    return {
+        env: {
+            g,
+            h,
+            p,
+            q,
+            c1, c2
+        },
+        proof:{
+            r
+        }
+    };
+};
+
+const verify_bond_equal_commits = (env, proof) => {
+    //general
+    const c1 = env.c1;
+    const c2 = env.c2;
+    const p = env.p;
+    const h = env.h;
+
+    //proof
+    const r = proof.r;
+
+    //get inversion of c2
+    const inv_c2 = powermod(c2, p - 2n, p);
+    
+    //check
+    return multi(c1, inv_c2, p) == powermod(h, r, p);
+};
+
 
 let p = 21888242871839275222246405745257275088548364400416034343698204186575808495617n;
 let g = 3n;
@@ -121,26 +162,22 @@ let h = powermod(g, 33n, p);//Hash(g);   //trusted setup
 //===========================================
 
 
-let secret = 69n;
+let secret = 2n;
+console.log("secret is", secret);   
 let t = 123n;
-let t2 = 456n;
 
-let c1 = commit(secret, t, g, h, p);
-let c2 = commit(secret, t2, g, h, p);
-
-let r = (t - t2 + q) % q;
-
-let inv_c2 = powermod(c2, p - 2n, p);
-
-let a = powermod(h, r, p);
-let b = multi(c1, inv_c2, p);
-const check = a == b;
-console.log(a, b, check);
+//circuit that makes
+let c = commit(secret, t, g, h, p);
+let c_squared = commit_squared(secret, t, g, h, p, q);
+let c_squared_bond = bond_equal_commits(c, t, c_squared.out.commit, c_squared.out.t, g, h, p, q);
+const constrain1 = verify_SquareProof(c_squared.env, c_squared.proof);
+const constrain2 = verify_bond_equal_commits(c_squared_bond.env, c_squared_bond.proof);
+console.log("is secret a bit?", constrain1, "&&",constrain2, "=",constrain1 && constrain2);
 
 /*
 let c1 = commit(secret, t, g, h, p);
-let c2 = commit_squared(secret, t, g, h, p);
+let c2 = commit_squared(secret, t, g, h, p, q);
 console.log(verify_SquareProof(c2.env, c2.proof));
-let c3 = commit_squared(c2.out.secret, c2.out.t, g, h, p);
+let c3 = commit_squared(c2.out.secret, c2.out.t, g, h, p, q);
 console.log(verify_SquareProof(c3.env, c3.proof));
 */
