3ba402d1b1fc7075d3b507a0ff0b94a555919fac
Dataram57 <dataram66657@gmail.com>
2025-10-05 19:11:41 +0200

    equal commitments check

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const add = (a, b, p) => (a + b) % p;
 const commit = (secret, rand, g, h, p) => multi(powermod(g, secret,p), powermod(h, rand, p), p);
 
 
+//returns a squared commitment
 const commit_squared = (secret, t1, g, h, p) => {
     //get current commit
     let c1 = commit(secret, t1, g, h, p);
@@ -101,8 +102,17 @@ const verify_SquareProof = (env, proof) => {
     return check1 && check2;
 };
 
+
 let p = 21888242871839275222246405745257275088548364400416034343698204186575808495617n;
 let g = 3n;
+
+
+let q = p - 1n;
+
+//check
+console.log(powermod(g, q, p)); //must
+
+
 let h = powermod(g, 33n, p);//Hash(g);   //trusted setup
 
 //console.log(powermod(g, p, p));
@@ -111,57 +121,26 @@ let h = powermod(g, 33n, p);//Hash(g);   //trusted setup
 //===========================================
 
 
-let secret = 69n;   // Alice’s secret
-let t1 = 123n;      // random blinding factor for c1
-let t2 = 456n;      // random blinding factor for c2
+let secret = 69n;
+let t = 123n;
+let t2 = 456n;
 
-// Commit to s
-let c1 = commit(secret, t1, g, h, p);
+let c1 = commit(secret, t, g, h, p);
+let c2 = commit(secret, t2, g, h, p);
+
+let r = (t - t2 + q) % q;
+
+let inv_c2 = powermod(c2, p - 2n, p);
+
+let a = powermod(h, r, p);
+let b = multi(c1, inv_c2, p);
+const check = a == b;
+console.log(a, b, check);
 
 /*
-// Commit to s^2
-//let c2 = commit(secret * secret % p, t2, g, h, p);
-let g2 = c1;        //THIS IS REQUIRED FOR THE VERIFIER TO BE SURE THAT THE NEW GENERATOR IS THE RESULT FOR
-let c2 = commit(secret, t2, g2, h, p);
-
-console.log("Commitment to s:", c1.toString());
-console.log("Commitment to s^2:", c2.toString());
-
-//===========================================
-//Proof of Equal Commitments with Different Binding Generators 
-
-let r1 = 555n;
-let r2 = 444n;
-let r3 = 222n;
-
-let c3 = commit(r1, r2, g, h, p);
-let c4 = commit(r1, r3, g2, h, p);
-
-
-let k = 666666n;    //can use fiat shamir
-
-
-let z1 = (multi(k, secret, p) + r1) % p;
-let z2 = (multi(k, t1, p) + r2) % p;
-let z3 = (multi(k, t2, p) + r3) % p;
-
-
-
-let check1 = multi(c3, powermod(c1, k, p), p) == commit(z1, z2, g, h, p);
-let check2 = multi(c4, powermod(c2, k, p), p) == commit(z1, z3, g2, h, p);
-console.log(check1, check2);
-
-
-//===========================================
-
-//simplify out commit
-
-let t3 = ((secret * t1) % p + t2) % p;
-secret = multi(secret, secret, p);
-console.log(commit(secret, t3, g, h, p));
+let c1 = commit(secret, t, g, h, p);
+let c2 = commit_squared(secret, t, g, h, p);
+console.log(verify_SquareProof(c2.env, c2.proof));
+let c3 = commit_squared(c2.out.secret, c2.out.t, g, h, p);
+console.log(verify_SquareProof(c3.env, c3.proof));
 */
-
-secret = 333333n;
-let c = commit_squared(secret, t1, g, h, p);
-console.log(c);
-console.log(verify_SquareProof(c.env, c.proof));
